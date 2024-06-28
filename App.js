201929b54bc05505dee986820f16d8a21641c55d
oktaf99 <oktaferi18@gmail.com>
2024-06-28 11:12:41 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+// Import axios, BASE_URL, dan API_KEY
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./src/constant";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import WeatherSearch from "./src/components/weatherSearch";
+import WeatherInfo from "./src/components/weatherInfo";
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState("");
+
+  const searchWeather = (location) => {
+    setStatus("loading");
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15; // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        setStatus('success')
+      })
+      .catch((error) => {
+        setStatus(error)
+      });
+  };
+
+
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size="large" />;
+      case "success":
+        return <WeatherInfo weatherData={weatherData} />;
+      case "error":
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        );
+      default:
+        return;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
+
+export default App;
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'a1fec523f6c324abece923ee046992ce';
